3dc6ada3519fe344c27f0ffa66ca6ee7b4ad18f0
João Soares <jsoareez@gmail.com>
2021-04-27 20:58:11 -0300

    refactoring code to work with typeorm

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,7 +7,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: 'nestjs',
   password: 'nestjs',
   database: 'taskmanagement',
-  entities: [__dirname + '/../**/*.entity.ts'],
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
-  logging: true,
 };
