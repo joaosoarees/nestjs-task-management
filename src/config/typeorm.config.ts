b6aa780d55544e0e4c9cdf27e33e6cd5d6bfa34a
João Soares <jsoareez@gmail.com>
2021-04-27 20:39:32 -0300

    typeorm with nestjs configuration

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'nestjs',
+  password: 'nestjs',
+  database: 'taskmanagement',
+  entities: [__dirname + '/../**/*.entity.ts'],
+  synchronize: true,
+  logging: true,
+};
