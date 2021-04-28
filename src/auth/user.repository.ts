cf31d5bf2a9dabdf6d1312bd09dabbb4a285d815
João Soares <jsoareez@gmail.com>
2021-04-28 20:49:59 -0300

    hashing password with bcrypt and store into database

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,6 +2,7 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
@@ -11,10 +12,12 @@ export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+
     const user = new User();
 
     user.username = username;
-    user.password = password;
+    user.password = await this.hashPassword(password, salt);
 
     try {
       await user.save();
@@ -25,5 +28,9 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return await bcrypt.hash(password, salt);
   }
 }
