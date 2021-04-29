038420263daa90f6de5bd81257ee4b89184ba2fb
João Soares <jsoareez@gmail.com>
2021-04-28 21:53:07 -0300

    relating users and tasks

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -2,10 +2,13 @@ import {
   BaseEntity,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { Task } from 'src/tasks/task.entity';
+import { type } from 'node:os';
 
 @Entity({
   name: 'users',
@@ -20,6 +23,9 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  @OneToMany((type) => Task, (task) => task.user, { eager: true })
+  tasks: Task[];
 
   async validatePassword(password: string): Promise<boolean> {
     const hash = await bcrypt.hash(password, this.password);
